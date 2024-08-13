ea01d20334b9167803f222f2a615a52795419410
denmarksdev <denmarksdev@gmail.com>
2024-08-12 23:11:19 -0300

    test(customer): casos de testes iniciais crud

---
+++ source-dotnet8/Customize/Customize.Tests/Functional/Customer/CustomerServiceTests.cs
@@ -0,0 +1,93 @@
+﻿namespace Customize.Tests.Functional.Customer
+{
+    public class CustomerServiceTests
+    {
+        /// <summary>
+        /// 1.1 - Deve validar os dados obrigatórios do cliente antes de salvar os dados.
+        /// </summary>
+        [Fact]
+        public void ShoudValidataDataBeforeSave()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 1.2 - Deve salvar os dados do cliente.
+        /// </summary>
+        [Fact]
+        public void ShouldSave()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 2.1 - Deve consultar clientes de forma paginada.
+        /// </summary>
+        [Fact]
+        public void ShouldList()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 2.2 - Deve consultar cliente por ID.
+        /// </summary>
+        [Fact]
+        public void ShouldGetByID()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 3.1 - Deve validar os dados obrigatórios do cliente antes de atualizar os dados.
+        /// </summary>
+        [Fact]
+        public void ShouldValidataDataBeforeUpdate()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 3.2 - Deve salvar os dados do cliente.
+        /// </summary>
+        [Fact]
+        public void ShouldUpdate()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 4.1 - Deve validar dados do cliente antes de excluir.
+        /// </summary>
+        [Fact]
+        public void ShouldValidataDataBeforeDelete()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        /// <summary>
+        /// 4.2 - Deve excluir os dados do cliente.
+        /// </summary>
+        [Fact]
+        public void ShouldDelete()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
