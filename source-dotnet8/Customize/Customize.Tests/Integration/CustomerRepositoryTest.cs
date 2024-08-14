d01411ab16b518509a7d5c79060943a73b7b895f
denmarksdev <denmarksdev@gmail.com>
2024-08-13 21:04:56 -0300

    test(integration): inicio testes de persitencia

---
+++ source-dotnet8/Customize/Customize.Tests/Integration/CustomerRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿namespace Customize.Tests.Integration
+{
+    public class CustomerRepositoryTest
+    {
+        [Fact]
+        public void ShouldSave() 
+        { 
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void ShouldUpdate()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void ShouldList()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void ShouldFind()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void ShouldDelete()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
